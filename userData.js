850868bcfc334855b5b9fd2cb649dc28c3dac93f
Prateek Santani <prateeksantani98@gmail.com>
2024-02-14 20:47:37 +0530

    final commit

---
+++ userData.js
@@ -0,0 +1,161 @@
+// User Data for dynamic addition of users in user list...........................
+const usersData = [
+  {
+    avatar:
+      "./assets/download.jpg",
+    name: "Eminem",
+    time: "10 min ago",
+    status: "Rapper",
+  },
+  {
+    avatar:
+      "./assets/Tesla_circa_1890.jpeg",
+    name: "Nicolas Tesla",
+    time: "10 min ago",
+    status: "Genius, Scientist,Legend",
+  },
+  {
+    avatar:
+      "./assets/Albert-Einstein-1947.webp",
+    name: "Albert Einstein",
+    time: "3 hours ago",
+    status: "Bulb Inventor",
+  },
+  {
+    avatar:
+      "./assets/modi.jpg",
+    name: "Narendra Modi",
+    time: "22 hours ago",
+    status: "Prime Minister of India",
+  },
+  {
+    avatar: "./assets/trump.jpg",
+    name: "Donald Trump",
+    time: "1 hour ago",
+    status: "American President",
+  },
+  {
+    avatar:
+      "./assets/stevejobs.1419962539.jpg",
+    name: "Steve Jobs",
+    time: "22 hours ago",
+    status: "Apple Founder",
+  },
+  {
+    avatar:
+      "./assets/kanye-west-balenciaga-cut-ties.webp",
+    name: "Kanye West",
+    time: "2 day ago",
+    status: "Music Producer",
+  },
+  {
+    avatar: "./assets/dhoni.webp",
+    name: "MS.Dhoni",
+    time: "Dec 10",
+    status: "Indian Cricket Team Captain, Farmer",
+  },
+  {
+    avatar:
+      "./assets/billie-eilish-blonde-hair-111523-678ec5787c254c4d89af81c7096b3161.jpg",
+    name: "Billie Eilish",
+    time: "Dec 8",
+    status: "Singer",
+  },
+  {
+    avatar: "./assets/weekend.jpg",
+    name: "Abel  Tesfaye",
+    time: "Dec 6",
+    status: "Pop Singer known as Weekend",
+  },
+  {
+    avatar:
+      "./assets/sonam-bajwa-11602402335.webp",
+    name: "Sonam Bajwa",
+    time: "April 11",
+    status: "Actress",
+  },
+  {
+    avatar:
+      "./assets/Mike_Tyson_Photo_Op_GalaxyCon_Austin_2023.jpg",
+    name: "Mike Tyson",
+    time: "Dec 2",
+    status: "Boxer",
+  },
+];
+
+// Json data ...........................................
+const JsonData = {
+  chat1: [
+    {
+      from: {
+        type: "user1",
+      },
+      msg: {
+        message: "Hi, Bro how you Doing?",
+      },
+    },
+    {
+      from: {
+        type: "user2",
+      },
+      msg: {
+        message:
+          "Hi, I am doing good bro. How about you?",
+      },
+    },
+    {
+      from: {
+        type: "user1",
+      },
+      msg: {
+        message:
+          "Lets catch up this weekend",
+      },
+    },
+    {
+      from: {
+        type: "user2",
+      },
+      msg: {
+        message: "Sure bro lets go out to watch the movie together",
+      },
+    },   
+  ],
+  chat2: [
+    {
+      from: {
+        type: "user1",
+      },
+      msg: {
+        message: "Hi, how can I help you?",
+      },
+    },
+    {
+      from: {
+        type: "user2",
+      },
+      msg: {
+        message:
+          "I have issue with my tesla car its not charging properly",
+      },
+    },
+    {
+      from: {
+        type: "user1",
+      },
+      msg: {
+        message:
+          "Sure! I will definitely help you with you query. Could you please confirm your email address  so that I can assist you better?",
+      },
+    },
+    {
+      from: {
+        type: "user2",
+      },
+      msg: {
+        message: "prateeksantani@gmail.com",
+      },
+    },    
+  ],
+  
+};
